2d58421e557a73e18437e1ff47c319fd08c2307a
김정연 <zoemfhs123@Naver.com>
2019-02-21 04:27:58 +0900

    uniform motion
    commit

--- uniform motion.cpp
+++ uniform motion.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <cmath>
 #include <windows.h>
-#include <math.h>
 
 using namespace std;
 
 // 점과 점사이의 거리를 반환하는 함수
-double distance(double pX, double pY, double mX, double mY)
+inline double distance(double pX, double pY, double mX, double mY)
 {
 	return sqrt(pow(pX - mX, 2) + pow(pY - mY, 2));
 }
@@ -17,13 +17,11 @@ int main()
 	double mouseX = 0, mouseY = 0;
 	double dist = 0;
 
+	cout << "input: playerX, playerY, mouseX, mouseY: ";
 	cin >> playerX >> playerY >> mouseX >> mouseY;
 
-	while (playerX != mouseX && playerY != mouseY)
+	while (dist = distance(playerX, playerY, mouseX, mouseY))	// 점과 점사이의 거리 값 갱신 (거리가 0이되면 루프 이탈)
 	{
-		// 점과 점사이의 거리 값 갱신
-		dist = distance(playerX, playerY, mouseX, mouseY);
-
 		// 플레이어지점과 마우스지점의 거리가 이동값보다 작은경우를 처리
 		if (fabs(mouseX - playerX) < (mouseX - playerX) / dist * speed
 			&& fabs(mouseY - playerY < (mouseX - playerX) / dist * speed))
